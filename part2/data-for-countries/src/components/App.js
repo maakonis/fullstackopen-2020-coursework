0046aabe86bdda8ff051d0bf1e5a0f838998852b
Mareks Zevalds <zevaldsmareks@gmail.com>
2020-04-13 15:21:01 +0100

    Complete Part 2 exercises 2.11 - 2.14

--- part2/data-for-countries/src/components/App.js
+++ part2/data-for-countries/src/components/App.js
@@ -2,48 +2,57 @@ import React from 'react';
 import axios from 'axios'
 
 
-const CountriesList = ({countries, filterTerm, weatherInfo}) => {
-    
+const CountriesList = ({ countries, filterTerm }) => {
+
   if (!filterTerm) {
     return <p>Enter search term.</p>
-  } 
-  
+  }
+
   if (countries.length === 1) {
-    const [ country ] = countries 
-    return <CountryBio country={country} weatherInfo={weatherInfo} />
+    const [country] = countries
+    return <CountryBio country={country} />
   } else if (countries.length >= 10) {
     return <p>Too many matches, specify another filter</p>
   }
-  
+
   return countries.map(country => {
     return (
       <div key={country.name}>
         <span>{country.name} </span>
-        <ToggleBio key={country} country={country} weatherInfo={weatherInfo} />
+        <ToggleBio key={country} country={country} />
       </div>
     )
   })
 }
 
-const CountryBio = ({country, weatherInfo}) => {
-  console.log('weatherInfo is in the countrybio', weatherInfo)
+const CountryBio = ({ country }) => {
+
+  const [weather, setWeather] = React.useState({})
+  const accessKey = process.env.REACT_APP_WEATHER_API_KEY
+
+  React.useEffect(() => {
+    axios
+      .get(`http://api.weatherstack.com/current?access_key=${accessKey}&query=${country.capital}&units=m`)
+      .then(response => setWeather(response.data))
+  }, [country, accessKey])
+  
   return (
     <div key={country.name}>
-        <h1>{country.name}</h1>
-        <p>capital {country.capital}</p>
-        <p>population {country.population}</p>
-        <h2>languages</h2>
-        {<ul>
-          {country.languages.map(language => <li key={language.name}>{language.name}</li>)}
-        </ul>}
-        <img src={country.flag} alt="Country Flag" width="100"/>
-        <WeatherInfo city={country.capital} />
-      </div>
+      <h1>{country.name}</h1>
+      <p>capital {country.capital}</p>
+      <p>population {country.population}</p>
+      <h2>languages</h2>
+      {<ul>
+        {country.languages.map(language => <li key={language.name}>{language.name}</li>)}
+      </ul>}
+      <img src={country.flag} alt="Country Flag" width="100" />
+      {(Object.keys(weather).length !== 0) ? <WeatherInfo weather={weather} /> : null}
+    </div>
   )
 }
 
-const ToggleBio = ({country, weatherInfo}) => {
-  const [ show, setNewShow] = React.useState(false)
+const ToggleBio = ({ country }) => {
+  const [show, setNewShow] = React.useState(false)
 
   return (
     <>
@@ -53,40 +62,26 @@ const ToggleBio = ({country, weatherInfo}) => {
   )
 }
 
-const WeatherInfo = ({city}) => {
-  console.log('run weatherinfo')
-  const [ weather, setWeather ] = React.useState({})
-  const accessKey = process.env.REACT_APP_WEATHER_API_KEY
-
-  React.useEffect(() => {
-    console.log('run effect hook for weather api')
-    axios
-    .get(`http://api.weatherstack.com/current?access_key=${accessKey}&query=${city}&units=m`)
-    .then(response => {
-      console.log('weather response data', response.data)
-      setWeather(response.data)
-    })
-  }, [])
-
+const WeatherInfo = ({ weather }) => {
   return (
-      <div>
-        <h2>Weather in {weather.location.name}</h2>
-        <strong>temperature: </strong><span>{weather.current.temperature} Celsius</span>
-        <strong>wind: </strong><span>{weather.current.wind_speed} m/s direction {weather.current.wind_dir} Celsius</span>
-      </div>
-    )
-  
+    <div>
+      <h2>Weather in {weather.location.name}</h2>
+      <p><strong>temperature: </strong>{weather.current.temperature} Celsius</p>
+      <img src={weather.current.weather_icons[0]} alt="Weather Icon" />
+      <p><strong>wind: </strong>{weather.current.wind_speed} m/s direction {weather.current.wind_dir}</p>
+    </div>
+  )
 }
 
 const App = () => {
-  const [ countries, setNewCountries] = React.useState([])
-  const [ filterTerm, setNewFilterTerm ] = React.useState('')
-  const [ filteredCountries, setNewFilteredCountries ] = React.useState([])
-  
+  const [countries, setNewCountries] = React.useState([])
+  const [filterTerm, setNewFilterTerm] = React.useState('')
+  const [filteredCountries, setNewFilteredCountries] = React.useState([])
+
   React.useEffect(() => {
     axios
       .get('https://restcountries.eu/rest/v2/all')
-      .then(response => setNewCountries(response.data)) 
+      .then(response => setNewCountries(response.data))
   }, [])
 
   const handleChange = (e) => {
--- part2/data-for-countries/src/index.js
+++ part2/data-for-countries/src/index.js
@@ -4,9 +4,10 @@ import App from './components/App';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
